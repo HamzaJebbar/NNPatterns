187637e0cdb9074c184645ceaafbe3d53d5923e8
root <doha.chemaou@etu.umontpellier.fr>
2020-02-19 10:22:57 +0100

    encrypting

--- filee.py
+++ filee.py
@@ -122,42 +122,75 @@ def Histogram(X,histname):
 X,X_1_0,y = readXy("iris_8_10_8_/iris_l1_8_l2_10_l3_8_.csv")
 X_1 = [X_1_0[i] for i in range(len(X_1_0)) if y[i]==1]
 X_0 = [X_1_0[i] for i in range(len(X_1_0)) if y[i]==0]
-#print("X_0 : \n",X_0)
-#print("\nX_1 :\n",X_1)
 
-# dictionnaire des différentes valeurs de X cryptées
-def encrypting(X_) : 
-	listOfX_ = []
-	encrypting_X_ = {}
+def listOfX_0(X_0) :
+	listOfX_0 = []
+	for x in range(len(X_0)) :
+		if not (X_0[x] in listOfX_0) :
+			listOfX_0.append(X_0[x])
+	return listOfX_0	
+
+#print(listOfX_0(X_0)) 
+'''
+def listOfX_1(X_1) :
+	listOfX_1 = []
+	for x in range(len(X_1)) : 
+		if not (X_1[x] in listOfX_1) :
+			listOfX_1.append(X_1[x])
+	return listOfX_1
+
+#print(listOfX_1(X_1))
+
+
+def encryptedX_0(listOfX_0) :
+	encrypted_X_0 = {}
 	key = 1
-	for x in range(len(X_)) : 
-		if not (X_[x] in listOfX_) : 
-			listOfX_.append(X_[x])
-			encrypting_X_[str(X_[x])] = str(key)
+	for x in range(len(listOfX_0)) : 
+		encrypted_X_0[str(key)] = listOfX_0[x]
+		key+=1
+	return encrypted_X_0
+#print(encryptedX_0(listOfX_0(X_0)))
+
+def encryptedX_1(listOfX_1) :
+	encrypted_X_1 = {}
+	key = 1
+	for x in range(len(listOfX_1)) : 
+		encrypted_X_1[str(key)] = listOfX_1[x]
+		key+=1
+	return encrypted_X_1
+#print(encryptedX_1(listOfX_1(X_1)))
+'''
+def encrypted_X_0_V1(X_0) : 
+	listOfX_0 = []
+	encrypted_X_0 = {}
+	key = 1
+	for x in range(len(X_0)) : 
+		if not (X_0[x] in listOfX_0) : 
+			listOfX_0.append(X_0[x])
+			encrypted_X_0[str(key)] = X_0[x]
 			key += 1
-	return encrypting_X_
+	return encrypted_X_0
+print("encrypted X_0 :\n",encrypted_X_0_V1(X_0))
 
-encrypting_X_0 = encrypting(X_0)
-encrypting_X_1 = encrypting(X_1)
-print("encrypting X_0 :\n", encrypting_X_0,"\nencrypting X_1 : \n", encrypting_X_1)
+def encrypted_X_1_V1(X_1) : 
+	listOfX_1 = []
+	encrypted_X_1 = {}
+	key = 1
+	for x in range(len(X_1)) : 
+		if not (X_1[x] in listOfX_1) : 
+			listOfX_1.append(X_1[x])
+			encrypted_X_1[str(key)] = X_1[x]
+			key += 1
+	return encrypted_X_1
+print("\nencrypted X_1 :\n",encrypted_X_1_V1(X_1))
 
-# liste X cryptée
-def X_to_encrypted_X(X_,encrypted_X_) :
-	X = []
-	for x in range(len(X_)) :
-		X.append(encrypted_X_[X_[x]])
-	return X
+encrypted_X_0 = encrypted_X_0_V1(X_0)
+encrypted_X_1 = encrypted_X_1_V1(X_1)
 
-encrypted_X_0 = X_to_encrypted_X(X_0,encrypting_X_0)
-encrypted_X_1 = X_to_encrypted_X(X_1,encrypting_X_1)
-print("encrypted X_0\n",encrypted_X_0)
-print("encrypted_X_1\n",encrypted_X_1)
 
-Histogram(encrypted_X_1,"X1.png")
-Histogram(encrypted_X_0,"X0.png")
+Histogram(list(encrypted_X_1_V1(X_1)),"X1.png")
+Histogram(list(encrypted_X_0_V1(X_0)),"X0.png")
 
-#Histogram(X_1,"X1.png")
-#Histogram(X_0,"X0.png")
 ## exemple d’utilisation 
-df=discretise_dataset('iris_8_10_8_/iris_l1_8_l2_10_l3_8_.csv',8)
+df=discretise_dataset('iris_8_10_8_/iris_l1_8_l2_10_l3_8_.csv',10)
 df.to_csv("iris_8_10_8_/iris_l1_8_l2_10_l3_8_disc.csv", sep=',', encoding='utf-8',index=False)
