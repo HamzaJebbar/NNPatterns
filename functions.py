2795aaaede90d64fa48cdd01d7d32b7a1a1a17f8
root <doha.chemaou@etu.umontpellier.fr>
2020-02-21 12:33:23 +0100

    organisation

--- functions.py
+++ functions.py
@@ -201,8 +201,75 @@ def hists_files(file,bins) : # "iris_8_10_8_/iris_l1_8_l2_10_l3_8_.csv" should b
 
 bins = [2,4,6,8,10]
 hists_files("iris_8_10_8_/iris_l1_8_l2_10_l3_8_.csv",bins)
+'''
+print(X_to_encrypted_X(df_list,encrypting_signature_value(df_list)))
+Histogram(X_to_encrypted_X(X_1_,encrypting_signature_value(df_list)),"X1cr.png")
+Histogram(enc_X_0,"X0cr.png")
+'''
+'''
+#print(len(df_list))
+#df.reset_index(drop = True)
+'''
+'''
+for x_x in range(len(df.columns)) :
+	print("x_x ",x_x)
+	xy = ""
+	for y_y in range(len(df)) :
+		xy += str(df[x_x][y_y])
+		#print("y_y ",y_y)
+		#print("df ",type(str(df[x_x][y_y])))
+	print(xy)
+'''
 
+#print(df)
+#print("01\n ",df[2][81])
+#print(len(pandas_core_frame_DataFrame_to_list(df)))
 
+'''
+def hists_files(file,bins) : # "iris_8_10_8_/iris_l1_8_l2_10_l3_8_.csv" file
+	X,X_1_0,y = readXy(file)
+	X_1 = [X_1_0[i] for i in range(len(X_1_0)) if y[i]==1]
+	X_0 = [X_1_0[i] for i in range(len(X_1_0)) if y[i]==0]
+
+	for x in range(len(bins)) :
+		name_of_generated_file = "iris_8_10_8_disc" + str(bins[x]) + ".csv"
+		name_of_pngHist_class0 = "X_0_disc" + str(bins[x]) + ".png"
+		name_of_pngHist_class1 = "X_1_disc" + str(bins[x]) + ".png"
+
+		df=discretise_dataset(file,bins[x])
+		print(pandas_core_frame_DataFrame_to_matrice(df))
+		df.to_csv(name_of_generated_file, sep=',', encoding='utf-8',index=False)
+		print(type(df))
+		print(df[0][81])
+		print(len(df.columns))
+'''
+'''
+	encrypting_sig_val = encrypting_signature_value(X_1_0) #dict encrypting
+	enc_X_0 = X_to_encrypted_X(X_0,encrypting_sig_val) 
+	enc_X_1 = X_to_encrypted_X(X_1,encrypting_sig_val)
+
+	Histogram(enc_X_1,"X1cr.png")
+	Histogram(enc_X_0,"X0cr.png") 
+
+	df=discretise_dataset(file,8)
+	file = "iris_8_10_8_/iris_l1_8_l2_10_l3_8_disc.csv"
+	df.to_csv(file, sep=',', encoding='utf-8',index=False)
+'''	
+'''		
+		encrypting_sig_val = encrypting_signature_value(X_1_0) #dict encrypting
+		enc_X_0 = X_to_encrypted_X(X_0,encrypting_sig_val) 
+		enc_X_1 = X_to_encrypted_X(X_1,encrypting_sig_val)
+
+		Histogram(enc_X_1,name_of_pngHist_class1)
+		Histogram(enc_X_0,name_of_pngHist_class0) 
+
+		
+		file = name_of_generated_file
+'''		
+'''
+bins = [2,4,6,8,10]
+hists_files("iris_8_10_8_/iris_l1_8_l2_10_l3_8_.csv",[8])
+'''
 
 
 
